7791fdb40e3a375f58cc4d1613c9a8eeb6e1b32e
apple <kenstjuil@icloud.com>
2025-03-05 11:39:58 +0300

    Добавили токен в main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
+from config import TOKEN
 
-bot = Bot(token="Your Token")
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
