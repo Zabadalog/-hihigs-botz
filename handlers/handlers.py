857f0346c30da28e3eb60803a8138b225d1df541
apple <kenstjuil@icloud.com>
2025-04-07 11:11:47 +0300

    Установила логирование по умолчанию

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,16 +1,23 @@
-__all__ = [
-    "router",
-]
-
+import logging
 from aiogram.filters import Command
 from aiogram import Router
+
+# Установка логирования по умолчанию
+logging.basicConfig(level=logging.INFO)
 
 router = Router()
 
 @router.message(Command("help"))
 async def process_start_command(message):
     await message.answer("ПОМОГИ!")
+    logging.info(f"User with id={message.from_user.id} need help")
 
 @router.message(Command(commands=["start", "status"]))
 async def process_start_command(message):
-    await message.reply(f"{message.from_user.id}, {message.from_user.username}")
+    await message.reply(f"ID{message.from_user.id}, User: {message.from_user.username}")
+    logging.info(f"User with id={message.from_user.id} launched the bot")
+
+@router.message()
+async def echo_message(message):
+    await message.answer(message.text)
+    logging.info(f"User with id={message.from_user.id} sent an unprocessable message")
