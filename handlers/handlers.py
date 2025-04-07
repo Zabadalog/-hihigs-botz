9919ebfe10d314af0ccd406b5f202a086647d455
linnerv <polinavershinskaya@gmail.com>
2025-04-07 17:50:31 +0300

    Сделала bot_commands.py и добавила callbacks в handlers.py

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,6 +1,7 @@
 import logging
 from aiogram.filters import Command
-from aiogram import Router
+from aiogram import Router, F
+from .keyboard import main_keyboard
 
 router = Router()
 
@@ -11,10 +12,14 @@ async def process_start_command(message):
 
 @router.message(Command(commands=["start", "status"]))
 async def process_start_command(message):
-    await message.reply(f"ID{message.from_user.id}, User: {message.from_user.username}")
+    await message.reply(f"ID{message.from_user.id}, User: {message.from_user.username}", reply_markup=main_keyboard)
     logging.info(f"User with id={message.from_user.id} launched the bot")
 
 @router.message()
 async def echo_message(message):
     await message.answer(message.text)
     logging.info(f"User with id={message.from_user.id} sent an unprocessable message")
+
+@router.callback_query(F.data.startswith("continue"))
+async def callback_continue(callback):
+    await callback.message.answer(text="Успешно вызван callback!")
